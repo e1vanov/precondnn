116cd0523499de373d02409f8339969253340810
Egor <s02210401@gse.cs.msu.ru>
2024-09-26 00:32:23 +0300

    Added band matrix generator and ds03

--- src/utils/toeplitz_generator.py
+++ src/utils/toeplitz_generator.py
@@ -42,3 +42,21 @@ def gen_symm_toeplitz_ul_ds_uniform(lows=[0.],
 
     torch.save(X_train, path + 'train.pt')
     torch.save(X_test, path + 'test.pt')
+
+def gen_symm_band_toeplitz_ul_ds_normal(stds=[1.],
+                                        d=32,
+                                        n_samples=10000,
+                                        test_size=0.1,
+                                        path='./'):
+
+    padded_stds = np.zeros(d)
+    padded_stds[:len(stds)] = np.array(stds)
+
+    X = np.vstack(list(normal_toeplitz_generator(stds, n_samples)))
+
+    X_train, X_test = train_test_split(X, test_size=test_size)
+
+    X_train, X_test = torch.tensor(X_train), torch.tensor(X_test)
+
+    torch.save(X_train, path + 'train.pt')
+    torch.save(X_test, path + 'test.pt')
